0d05b0989795c79b16a81d62349dea4017589ed9
Samuel Lazareanu <52662032+SamuraiPolix@users.noreply.github.com>
2023-04-16 23:47:24 +0300

    add TOPIC option and fitness + organize files

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import post_handler
 import helper_images
 
+# Available topics: christian, fitness
+TOPIC = "christian"
+
 # Define the paths and values to everything
 number_of_posts = 119
-images_folder = "C:/Bots/ChristianPostMaker/sources/images"
+images_folder = f"C:/Bots/ChristianPostMaker/sources/images/{TOPIC}"
 images_folder_cropped = f"{images_folder}/cropped"
 images_folder_cropped_darken = f"{images_folder_cropped}/darken"
-text_file = "C:/Bots/ChristianPostMaker/sources/quotes.txt"
+text_file = f"C:/Bots/ChristianPostMaker/sources/text_data/{TOPIC}.txt"
 font_dir = "C:/Users/samla/AppData/Local/Microsoft/Windows/Fonts/MouldyCheeseRegular-WyMWG.ttf"
-output_folder = "C:/Bots/ChristianPostMaker/customers"
+output_folder = f"C:/Bots/ChristianPostMaker/customers/{TOPIC}"
 logo_file = "C:/Bots/ChristianPostMaker/sources/logo.png"
 customer_name = "no_logo"
 
