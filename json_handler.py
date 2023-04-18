723830692ed2d99785d601aa34df0a4e4b7358af
Samuel Lazareanu <52662032+SamuraiPolix@users.noreply.github.com>
2023-04-18 19:43:58 +0300

    logo changes

--- json_handler.py
+++ json_handler.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 def get_data(json_file):
@@ -7,3 +8,9 @@ def get_data(json_file):
     verses: str = jsonData['verses']
     refs: str = jsonData['references']
     return verses, refs
+
+
+def txt_to_json(text_file, output_folder):
+    # TODO FINISH TEXT TO JSON
+    with open(f'{text_file}', 'r', encoding='utf-8') as f:
+        jsonData = json.load(f)
--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import helper
 
 project_dir = os.getcwd().replace("\\", "/")
 # Available topics: christian, fitness
-TOPIC = "fitness"
+TOPIC = "christian"
 
 # Define the paths and values to everything
-number_of_posts = 119
+number_of_posts = 81
 images_folder = f"{project_dir}/sources/images/{TOPIC}"
 images_folder_cropped = f"{images_folder}/cropped"
 images_folder_cropped_darken = f"{images_folder_cropped}/darken"
@@ -16,19 +16,19 @@ text_file = f"{project_dir}/sources/text_data/{TOPIC}.txt"
 font_dir = "C:/Users/samla/AppData/Local/Microsoft/Windows/Fonts/MouldyCheeseRegular-WyMWG.ttf"
 output_folder = f"{project_dir}/customers/{TOPIC}"
 logo_file = f"{project_dir}/sources/logo.png"
-customer_name = "no_logo"
+customer_name = "everythingod"
 
 
 if __name__ == "__main__":
     # helper.create_new_topic_dirs(TOPIC, project_dir)
 
-    helper.cut_images_new(images_folder, images_folder_cropped)
+    # helper.cut_images_new(images_folder, images_folder_cropped)
     # helper.darken_images(images_folder_cropped, images_folder_cropped_darken)
 
     # LOGO
-    # post_handler.create_posts(images_folder=images_folder_cropped_darken, text_file=text_file,
-    #                      font_dir=font_dir, output_folder=output_folder,
-    #                      logo_file=logo_file, customer_name=customer_name, number_of_posts=number_of_posts)
+    post_handler.create_posts(images_folder=images_folder_cropped_darken, text_file=text_file,
+                         font_dir=font_dir, output_folder=output_folder,
+                         logo_file=logo_file, customer_name=customer_name, number_of_posts=number_of_posts)
 
     # NO LOGO
     # post_handler.create_posts(images_folder=images_folder_cropped_darken, text_file=text_file,
